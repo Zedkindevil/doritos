21d7faf1b53ca3d2493ffa269d5ef61a542434d3
Zedkindevil <94899615+Zedkindevil@users.noreply.github.com>
2022-03-01 15:59:25 +0530

    Update google.py

--- Yuriko/modules/google.py
+++ Yuriko/modules/google.py
@@ -297,7 +297,7 @@ __help__ = """
 
 ✗ Yuriko - `<query> Yuriko answers the query`
 
-💡 Ex: `Yuriko where is Japan?`
+💡 Ex: `kindevils manager where is Japan?`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: 𝐊𝐢𝐧𝐝 𝐄𝐯𝐢𝐥𝐬!*
 """
