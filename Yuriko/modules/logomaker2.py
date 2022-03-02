86d771393378a881a0205c0abc618f4e3fbb6235
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-02 15:00:31 +0530

    Update logomaker2.py

--- Yuriko/modules/logomaker2.py
+++ Yuriko/modules/logomaker2.py
@@ -268,7 +268,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "black"
     shadowcolor = "blue"
-    fnt = glob.glob("./TGNRobot/utils/Logo/*")
+    fnt = glob.glob("./Yuriko/resources/fonts/*")
     randf = random.choice(fnt)
     font = ImageFont.truetype(randf, 120)
     w, h = draw.textsize(text, font=font)
